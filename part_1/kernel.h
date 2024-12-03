9475e6c697aa6162e86cc4d1bfeda229f28a2ef3
Jesse Hudson <jesse@easyversal.com>
2024-12-03 22:11:21 +0000

    Changed type to int

--- part_1/kernel.h
+++ part_1/kernel.h
@@ -2,6 +2,6 @@
 
 #include <adf.h>
 
-using AIE_BUFFER_TYPE = int32;
+using AIE_BUFFER_TYPE = int;
 
 void AddOne(adf::input_buffer<AIE_BUFFER_TYPE> &restrict data_in, adf::output_buffer<AIE_BUFFER_TYPE> &restrict data_out);
