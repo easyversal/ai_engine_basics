2405b77e92b46862fbcec2101332fb569b122212
Jesse Hudson <jessehudson@Jesses-MacBook-Air.local>
2024-12-03 19:39:58 -0800

    Added graph simulation code

--- src/graph.cpp
+++ src/graph.cpp
@@ -24,6 +24,50 @@ SimpleGraph::SimpleGraph() : m_kernel{adf::kernel::create(AddOne)},
 #if defined(__AIESIM__) || defined(__X86SIM__)
 int main()
 {
-  return 0;
+  constexpr auto ITERATION{4};
+  constexpr auto BLOCK_BYTES{ITERATION * AIE_BUFFER_SIZE * sizeof(AIE_BUFFER_TYPE)};
+  auto data_in{(AIE_BUFFER_TYPE *)adf::GMIO::malloc(BLOCK_BYTES)};
+  auto data_out{(AIE_BUFFER_TYPE *)adf::GMIO::malloc(BLOCK_BYTES)};
+
+  std::cout << "GMIO malloc completed\n";
+
+  AIE_BUFFER_TYPE data_value{};
+  for (auto gmio_index{0}; gmio_index < ITERATION * AIE_BUFFER_SIZE; ++gmio_index)
+  {
+    data_in[gmio_index] = data_value;
+    ++data_value;
+  }
+
+  simple_graph.init();
+  simple_graph.m_gmio_in.gm2aie_nb(data_in, BLOCK_BYTES);
+  simple_graph.run(ITERATION);
+  simple_graph.m_gmio_out.aie2gm(data_out, BLOCK_BYTES);
+  simple_graph.end();
+
+  auto errors{0};
+  for (auto ii{0}; ii < ITERATION * AIE_BUFFER_SIZE; ++ii)
+  {
+    if (data_out[ii] != data_in[ii] + 1)
+    {
+      std::cout << "Error: dout[" << ii << "] = " << data_out[ii] << ", gold = " << data_in[ii] + 1 << '\n';
+      ++errors;
+    }
+  }
+
+  std::cout << "GMIO transactions finished\n";
+
+  adf::GMIO::free(data_in);
+  adf::GMIO::free(data_out);
+
+  if (errors == 0)
+  {
+    std::cout << "TEST PASSED!\n";
+  }
+  else
+  {
+    std::cout << "ERRORS!\n";
+  }
+
+  return errors;
 }
 #endif
