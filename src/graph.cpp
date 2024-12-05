d882f6920cadf4a702d14f35d424de816add6da7
Jesse Hudson <jesse@easyversal.com>
2024-12-05 14:48:46 -0800

    Rearranged constructor body

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,23 +2,22 @@
 #include "constants.h"
 #include "graph.h"
 
+SimpleGraph simple_graph{};
+
 constexpr auto BURST_LENGTH{64};
 constexpr auto BANDWIDTH{1000};
-
-SimpleGraph simple_graph{};
 
 SimpleGraph::SimpleGraph() : m_kernel{adf::kernel::create(AddOne)},
                              m_gmio_out{adf::output_gmio::create(BURST_LENGTH, BANDWIDTH)},
                              m_gmio_in{adf::input_gmio::create(BURST_LENGTH, BANDWIDTH)}
 {
-  adf::connect(m_gmio_in.out[0], m_kernel.in[0]);
-  adf::connect(m_kernel.out[0], m_gmio_out.in[0]);
   adf::source(m_kernel) = "add_one.cpp";
-
   adf::dimensions(m_kernel.in[0]) = {AIE_BUFFER_SIZE};
   adf::dimensions(m_kernel.out[0]) = {AIE_BUFFER_SIZE};
-
   adf::runtime(m_kernel) = 0.9;
+
+  adf::connect(m_gmio_in.out[0], m_kernel.in[0]);
+  adf::connect(m_kernel.out[0], m_gmio_out.in[0]);
 }
 
 #if defined(__AIESIM__) || defined(__X86SIM__)
